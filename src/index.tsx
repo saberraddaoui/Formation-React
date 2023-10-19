e2e92c75907aab6ded1a67d844431c45c0889033
saberraddaoui <saberraddaoui2001@gmail.com>
2023-10-19 16:18:15 +0200

    first day on react

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+//import '~bootstrap/scss/bootstrap.scss';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
